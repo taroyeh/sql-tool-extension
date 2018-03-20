a80fc2864812266200364d6892ef7f1f37339b6a
Taro.C.Yeh <ty56@WT41TECH17.buyabs.corp>
2018-03-20 15:07:38 +0800

    Fix problem: Sometimes the extension is load fail.

--- sql-tool-extension/start.js
+++ sql-tool-extension/start.js
@@ -5,18 +5,30 @@
     newScript.appendChild(inlineScript); 
     (document.head || document.documentElement).appendChild(newScript);
 
-    // Execute scripts
+    // Inject scripts
+    var loaded = [];
     var scripts = [
         "codemirror-sql/codemirror.js",
-        "codemirror-sql/mirrorframe.js",
-        "main.js"
+        "codemirror-sql/mirrorframe.js"
     ];
     for (var i = 0; i < scripts.length; i++) {
+        loadScript(scripts[i]);
+    }
+
+    var timer = setInterval(function() {
+        if (loaded.length == scripts.length) {
+            loadScript("main.js");
+            clearInterval(timer);
+        }
+    }, 100);
+
+    function loadScript(path) {
         var e = document.createElement('script');
-        e.src = chrome.extension.getURL(scripts[i]);
+        (document.head || document.documentElement).appendChild(e);
+        e.src = chrome.extension.getURL(path);
         e.onload = function() {
+            loaded.push(e);
             this.remove();
         };
-        (document.head || document.documentElement).appendChild(e);
     }
 })();

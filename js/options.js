678fac3a67b426c5c98f57278b65d3ccb8318f3c
Taro.C.Yeh <ty56@WT41TECH17.buyabs.corp>
2018-03-23 15:11:52 +0800

    Finished options UI.

--- js/options.js
+++ js/options.js
@@ -1,17 +1,88 @@
-$("#tabs").tabs().css({
-   'min-height': '316px',
-   'overflow': 'auto'
-});
+(function() {
+    $("#tabs").tabs().css({
+       "min-height": "348px",
+       "overflow": "auto"
+    });
 
-$("button").button();
+    $("button").button();
 
-$("#btnReset").click(function(e) {
-    $("#fmOptions")[0].reset();
-    e.preventDefault();
-});
+    $("#btnReset").click(function(e) {
+        e.preventDefault();
+        $("#fmOptions")[0].reset();
+    });
 
-$("#btnSave").click(function(e) {
-    // TODO: save options
-    e.preventDefault();
-    window.close();
-});
+    $("#btnSave").click(function(e) {
+        e.preventDefault();
+
+        var options = {};
+        $(".options-field").each(function() {
+            var $this = $(this);
+            var name = $this.prop("name");
+            if ($this.prop("type").toLowerCase() == "checkbox") {
+                options[name] = $this.is(":checked");
+            } else {
+                options[name] = $this.val();
+            }
+        });
+
+        chrome.storage.sync.set(options, function() {
+            var $divMessage = $("#divMessage");
+            $divMessage.text("Options saved.");
+            setOptionsFields(options);
+            setTimeout(function() {
+                $divMessage.text("");
+            }, 1000);
+        });
+    });
+
+    chrome.storage.sync.get({
+        alternate_color: true,
+        colorful_sql: true,
+        f5_execute: true,
+        highlight_cursor_row: true,
+        selectable_row: true,
+        show_cell_title: true,
+        show_row_number: true,
+        editor_font_size: 12
+    }, setOptionsFields);
+
+    function setOptionsFields(options) {
+        $(".options-field").each(function() {
+            var $this = $(this);
+            var name = $this.prop("name");
+
+            if (!options.hasOwnProperty(name)) {
+                return;
+            }
+            var value = options[name];
+
+            if ($this.prop("type").toLowerCase() == "checkbox") {
+                if (value) {
+                    $this.attr("checked", "checked");
+                } else {
+                    $this.removeAttr("checked");
+                }
+                return;
+            }
+
+            if ($this.prop("tagName").toLowerCase() == "select") {
+                $this.find("option").each(function() {
+                    var $option = $(this);
+                    if ($option.val() == value) {
+                        $option.attr("selected", "selected");
+                    } else {
+                        $option.removeAttr("selected");
+                    }
+                });
+                return;
+            }
+
+            if ($this.prop("tagName").toLowerCase() == "textarea") {
+                $this.prop("innerHTML", value);
+                return;
+            }
+
+            $this.val(options[name]);
+        });
+    }
+})();
